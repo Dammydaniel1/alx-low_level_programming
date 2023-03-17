da9ec9858f8be710151e842bfe2c0c9ef3da29eb
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-17 12:29:42 -0700

    a c program that prints a specific text using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
-	char String[] = "Programming is like building a multilingual puzzle";
+	char String[] ="\"Programming is like building a multilingual puzzle\n" ;
+	
 	puts(String);
     	return (0);
 }
