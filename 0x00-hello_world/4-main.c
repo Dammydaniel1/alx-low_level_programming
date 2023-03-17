7323c836a434fda9973cdf007c440a0b9efbfedc
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-17 11:50:10 -0700

    using puts()

---
+++ 0x00-hello_world/4-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char String[] = "Programming is like building a multilingual puzzle";
+	puts(String);
+    	return (0);
+}
