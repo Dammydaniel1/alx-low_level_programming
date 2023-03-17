b9c2cc674dfaaa5921c56013cc4e5289f0ca7062
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-17 12:18:00 -0700

    a c program that prints a specific text using puts\(\)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc 4-main.c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char String[] = "Programming is like building a multilingual puzzle";
+	puts(String);
+    	return (0);
+}
