47a4fb3fd01a24c203f8acfd5560160332747efc
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-18 08:53:54 -0700

    Printing texts using printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
+/**An alternative way of displaying output
  *Printing with printf()
  */
+/** A main is required for c to run*/
 int main(void)
 {
+/** Using printf directly with the text to display*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+/**To return after execution*/
 	return(0);
 }
