7e0b3180d15c9aa6e32f39c0b1310dcaa7ccd365
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-18 10:48:11 -0700

    Updating betty doc style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- * a c-program that prints a certain text followed by a new line to the standard error
- * 
+ * prints a certain text to the standard error
  * always returns 1
  */
 int main(void)
+/*main*/
 {
 /*function to call:*/
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return(1);
+/*always 1*/
 }
-
