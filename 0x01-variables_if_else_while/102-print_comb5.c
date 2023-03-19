559ba6c1a4ac8f19ebbc33be889caa72d2bccdbf
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-19 05:45:42 -0700

    a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+/*
+ * Filename: 102-print_comb5.c
+ * Auth: Damilola
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digits1, digits2;
+
+	/*unique stratification*/
+	for (digits1 = 0; digits1 <= 98; digits1++)
+	{
+		for (digits2 = digits1 + 1; digits2 <= 99; digits2++)
+		{
+			putchar((digits1 / 10) + '0');
+			putchar((digits1 % 10) + '0');
+			putchar(' ');
+			putchar((digits2 / 10) + '0');
+                        putchar((digits2 % 10) + '0');
+
+			if (digits1 == 98 && digits2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	/*new line insertion*/
+	putchar('\n');
+
+	/*always 0*/
+	return (0);
+}
