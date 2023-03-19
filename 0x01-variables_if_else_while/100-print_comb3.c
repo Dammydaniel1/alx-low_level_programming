60513782c7f81eb46d8c6799de296a93e70a48f2
Dammydaniel1 <dammydaniel1@gmail.com>
2023-03-19 05:08:52 -0700

    a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+/*
+ * Filename: 100-print_comb3.c
+ * Auth: Damilola
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	/*unique stratification*/
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	/*new line insertion*/
+	putchar('\n');
+
+	/*always 0*/
+	return (0);
+}
