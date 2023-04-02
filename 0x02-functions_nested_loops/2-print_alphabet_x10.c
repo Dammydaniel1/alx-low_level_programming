b7615342f36ad65c48290f6594af6b36ee9e3254
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-02 04:17:38 -0700

    declaring the function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,32 @@
  * Filename: 2-print_alphabet_x10.c
  * Author: Damilola Tijani
  */
+
 #include "main.h"
 
-/**
- * function that prints 10 times the alphabet, in lowercase, followed by a new line
- * Always return zero
- */
+/*a function that produces the english alphabet in lower case*/
+void  print_alphabet_lower_case(void)
+{
+        char letter;
+
+        /*each english alphabet*/
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                putchar(letter);
+        }
+}
+
+/*looping alphabet ten times*/
+void print_alphabet_x10(void)
+{
+        int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet_lower_case();
+		putchar('\n')
+	}
+
 
 int main(void)
 {
