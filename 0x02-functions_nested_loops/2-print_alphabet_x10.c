e0dc267cfd327d301e6fba550bb99e96a060fb58
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-02 14:10:11 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -29,8 +29,9 @@ void  print_alphabet(void)
  */
 void print_alphabet_x10(void)
 {
+	int i;
 	/*10 times iteration*/
-	for (int i = 0, i < 10, i++)
+	for (i = 0, i <= 9, i++)
 	{
 		print_alphabet();
 	}
