cc3594b3dfeb22b9bf027965ac9262db2055661e
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-02 03:38:34 -0700

    english alphabet lower case times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/**
+ * Filename: 2-print_alphabet_x10.c
+ * Author: Damilola Tijani
+ */
+#include "main.h"
+
+/**
+ * function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * Always return zero
+ */
+
+int main(void)
+{
+	/*calling on the function from main*/
+	print_alphabet_x10();
+
+	/*always return zero*/
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,31 @@
+/**
+ * Filename: main.h
+ * Author: Damilola Tijani
+ *
+ * Adding all headers and functions
+ */
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <stdio.h>
 
-/**
- * adding the standard headers
- */
+
+/*a function that produces the english alphabet in lower case ten times*/
+
+void print_alphabet_x10(void)
+{
+	int i
+	char letter
+
+	/*iterating 10 times*/
+	for (i = 0, i < 10, i++)
+	{
+		/*each round of alphabet*/
+		for (letter >= 'a',letter <= 'z', letter++)
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+}
