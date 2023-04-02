fb5f07b28dd1fba1b2e0e9e095af7ab05c32e065
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-02 04:29:03 -0700

    function needed to be in main as done earlier

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,29 +5,6 @@
 
 #include "main.h"
 
-/*a function that produces the english alphabet in lower case*/
-void  print_alphabet_lower_case(void)
-{
-        char letter;
-
-        /*each english alphabet*/
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
-}
-
-/*looping alphabet ten times*/
-void print_alphabet_x10(void)
-{
-        int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		print_alphabet_lower_case();
-		putchar('\n');
-	}
-}
 
 int main(void)
 {
