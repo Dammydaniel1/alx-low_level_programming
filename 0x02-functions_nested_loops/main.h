11fef7460b54b43822424dbdbd15843bde89a34e
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-02 04:04:26 -0700

    realised i did not need to declare the function on my own. hence, i deleted it

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,29 +9,3 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdio.h>
-
-
-/*a function that produces the english alphabet in lower case*/
-void  print_alphabet_lower_case(void)
-{
-	char letter;
-
-	/*each round of english alphabet in lower case*/
-	for (letter = 'a'; letter<= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-}
-
-/*looping through the english alphabet ten times*/
-void print_alphabet_x10(void)
-{
-	int i;
-
-	/*iterating 10 times*/
-	for (i = 0; i < 10; i++)
-	{
-		print_alphabet_lower_case();
-		putchar('\n');
-	}
-}
