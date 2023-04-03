6c0dcb0b7774b33cdb7dfb0ffd85674f8cb0b2ab
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-03 16:46:36 -0700

    multiples of 3 and 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * Description:
+ * 
+ * It prints natural numbers that are multiples of 3 or 5 below 1024
+ *
+ * Always return: 0.
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
