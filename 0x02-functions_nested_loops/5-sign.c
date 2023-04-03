6372b7fab6f42c7a0e594511380e9b0c87ac1ff7
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-03 13:37:06 -0700

    print_sign_updated revieweing order of function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,17 +18,17 @@ int print_sign(int i)
 {
 	if (i > 0)
 	{
-		putchar(43);
 		return (1);
+		putchar(43);
 	}
 	else if (i < 0)
         {
-                putchar(45);
-                return (-1);
-        }
+		return (-1);
+		putchar(45);
+	}
 	else
 	{
-		putchar(48);
 		return (0);
+		putchar(48);
 	}
 }
