1a34a9b25672b8436dbcb35935d03ad59ad2c191
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-03 13:51:03 -0700

    print sign using _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,17 +18,17 @@ int print_sign(int i)
 {
 	if (i > 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (1);
 	}
 	else if (i < 0)
         {
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 	else
 	{
-		putchar(48);
+		_putchar(48);
 		return (0);
 	}
 }
