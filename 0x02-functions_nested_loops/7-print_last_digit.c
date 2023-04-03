9bcdae18954ea58b450c852282af3a46e629d22a
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-03 14:45:44 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,14 +18,14 @@ int print_last_digit(int i)
 {
 	int last_digit = i % 10;
 
-	if (i >= 0)
+	if (last_digit >= 0)
 	{
 		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 	else
 	{
-		i = i * -1;
+		last_digit = last_digit * -1;
 		_putchar(last_digit + '0');
 		return (last_digit);
 	}
