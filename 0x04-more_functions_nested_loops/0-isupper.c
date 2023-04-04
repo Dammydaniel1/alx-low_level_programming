191a0ef6653464d140e6ee5f2976641890998f95
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-04 13:12:04 -0700

    check for upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * Description:
+ * a function that checks for uppercase character
+ *
+ * returns 1 if uppercase
+ * returns 0 if otherwise
+ */
+
+int _isupper(int c);
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);	
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Other functions
+ */
+
+/*checks for upper case*/
+int _isupper(int c);
