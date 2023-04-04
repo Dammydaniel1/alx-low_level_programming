db66b60602743230ba7ac7994acedd68c3801719
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-04 14:05:43 -0700

    multiplies two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description:
+ *
+ * a function that mtiplies two integers
+ *
+ * return product
+ */
+
+int mul(int a, int b)
+{
+	int z = a * b;
+
+	return (z);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,6 @@ int _isupper(int c);
 
 /*checks for digits*/
 int _isdigit(int c);
+
+/*multiplies two integers*/
+int mul(int a, int b);
