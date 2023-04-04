0cfbb4b2d955afc50c335cbbf2e9b2cdd42504d8
Dammydaniel1 <dammydaniel1@gmail.com>
2023-04-03 17:03:46 -0700

    fibonacci 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * Description:
+ * It sums up even  fibonacci numbers less than 4000000
+ *
+ * Always return: 0.
+ */
+
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+
+		if (k % 2 == 0)
+			sum += k;
+
+		j = k - j;
+
+		++i;
+	}
+
+	printf("%ld\n", sum);
+	return (0);
+}
+
